88775f8eaa964fd2e4aa807927fc332fcda9b1f7
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-12 13:09:13 +0000

    pycode checked the file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,15 +11,15 @@ class FileStorage:
     def all(self,  cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not none:
-            if type(cls) == str:
+            if isinstance(cls, str):
                 cls = eval(cls)
                 cls_dict = {}
-                for key,value in self.__objects.items():
-                    if type(value) == cls:
+
+                for key, value in self.__objects.items():
+                    if isinstance(value, cls):
                         cls_dict[key] = value
                     return cls_dict
             return self.__objects
-                    
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -37,9 +37,9 @@ class FileStorage:
     def delete(self, obj=None):
         """deletes an existing element"""
         if obj:
-            key = "{}{}".format(type(obj).__name__,obj.id)
+            key = "{}{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
-    
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -60,6 +60,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
