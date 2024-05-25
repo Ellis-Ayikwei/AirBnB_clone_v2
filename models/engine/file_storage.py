a8b20169d5667ace9cd51f5cae0fa3d8e02655b6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-25 00:46:03 +0000

    edite dthe pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 import json
 import shlex
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
@@ -21,7 +22,7 @@ class FileStorage:
             return (dic)
         else:
             return self.__objects
-        
+   
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
