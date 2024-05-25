872b46144fc5d6f67eea26e0df391ee503636e0a
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-25 00:43:50 +0000

    city and db update

--- models/state.py
+++ models/state.py
@@ -3,10 +3,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 import models
 from models.city import City
-import shlex
+from os import getenv
+
 
 
 class State(BaseModel, Base):
@@ -14,5 +15,14 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
-    
-  
+
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """get a  list of cities if engine equals db"""
+            city_list=[]
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
